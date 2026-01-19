8e61ec0c855f732c4e70805924623887a1cb3e95
Hcnn2 <hcnn2@kent.ac.uk>
2026-01-19 14:10:07 +0000

    1

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,15 @@
 import math  
 
 def main():
-#TO DO  
+    A = float(input('A='))
+    B = float(input('B='))
+    pythag(A,B)
 
 def pythag(A,B):
-#TO DO  
+    A1 = A**2
+    B1 = B**2
+    C1 = A1 + B1
+    C = C1**0.5
+    print(C)
 
 main()
